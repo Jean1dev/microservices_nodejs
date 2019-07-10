6c0c410a36ed2a174fc180e1e916fd6e3ea55644
Jean <jeanlucafp@gmail.com>
2019-07-09 21:25:42 -0300

    bash scritps

---
+++ deploy.bash
@@ -0,0 +1,34 @@
+echo "iniciando"
+echo "iniciando gateway"
+cd module-gateway
+npm i
+APP_NAME="GATEWAY"
+pm2 start npm --name $APP_NAME -- run production
+
+cd ..
+echo "iniciando comunicacao"
+cd module-comunicacao
+npm i
+touch .env
+APP_NAME="comunicacao"
+pm2 start npm --name $APP_NAME -- run production
+
+cd ..
+echo "iniciando notify"
+cd module-notify
+npm i
+touch .env
+echo "// RENOMEAR PARA .env e colocar os valores
+module.exports = {
+    email: 'jeanlucafp@gmail.com',
+    senha: 'jeanlucaObscure345',
+}" > .env
+APP_NAME="notify"
+pm2 start npm --name $APP_NAME -- run production
+
+cd ..
+echo "iniciando integracao"
+cd module-integracao
+npm i
+APP_NAME="integracao"
+pm2 start npm --name $APP_NAME -- run production
