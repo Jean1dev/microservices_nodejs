676871af6f16e84b0ce6a2cf1a401fc19c77a6c5
Jean <jeanlucafp@gmail.com>
2019-07-13 17:50:44 -0300

    https

--- module-gateway/index.js
+++ module-gateway/index.js
@@ -1,5 +1,7 @@
 //index.js
-var http = require('http');
+const fs = require('fs');
+const http = require('http');
+const https = require('https')
 const express = require('express')
 const httpProxy = require('express-http-proxy')
 const app = express()
@@ -8,9 +10,10 @@ var logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors')
 const port = process.env.PORT || 8080
+const production = process.env.PROD || false
 
 app.use(cors())
-app.use(require('./routes')) 
+app.use(require('./routes'))
 app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
@@ -20,3 +23,38 @@ app.use(cookieParser());
 var server = http.createServer(app);
 server.listen(port);
 console.log(`module-gateway`, port)
+
+if (production) {
+    // Certificate
+    //https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
+    /**
+     * IMPORTANT NOTES:
+    - Congratulations! Your certificate and chain have been saved at:
+   /etc/letsencrypt/live/rocketenvios.tk/fullchain.pem
+   Your key file has been saved at:
+   /etc/letsencrypt/live/rocketenvios.tk/privkey.pem
+   Your cert will expire on 2019-10-11. To obtain a new or tweaked
+   version of this certificate in the future, simply run certbot
+   again. To non-interactively renew *all* of your certificates, run
+   "certbot renew"
+    - If you like Certbot, please consider supporting our work by:
+
+     Donating to ISRG / Let's Encrypt:   https://letsencrypt.org/donate
+     Donating to EFF:                    https://eff.org/donate-le
+     */
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/rocketenvios.tk/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/rocketenvios.tk/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/rocketenvios.tk/chain.pem', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(8080, () => {
+        console.log('HTTPS Server running on port 8080');
+    });
+
+}
